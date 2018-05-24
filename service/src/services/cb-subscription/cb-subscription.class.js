c64a0f8c374fac303a35d7f51fec394037b659b1
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-05-24 17:57:28 +0530

    some issue solve related to store subscription details locally

--- service/src/services/cb-subscription/cb-subscription.class.js
+++ service/src/services/cb-subscription/cb-subscription.class.js
@@ -1,6 +1,12 @@
 const config = require('../config.js');
+const axios = require('axios');
 let async = require('asyncawait/async');
 let await = require('asyncawait/await');
+let rp = require('request-promise');
+let _ = require('lodash');
+let moment = require('moment');
+
+moment().format();
 
 let transaction_id;
 let transObj = {
@@ -103,21 +109,112 @@ let retrieveSubscription = function (id, params) {
 };
 
 let createSubscription = async(function (data, params) {
+  //INITIATE NEW TRANSACTION 
   transaction_id = await (initiateTransaction(data, params));
-  if (transaction_id.hasOwnProperty('error')) {
+  if (transaction_id.hasOwnProperty('api_error_code')) {
     return transaction_id;
   }
   console.log('transaction_id : ', transaction_id);
 
-  return config.chargebee.subscription.create(data).request(function (error, result) {
+  //CREATE SUBSCRIPTION USING CHARGEBEE API
+  return config.chargebee.subscription.create(data).request(async (function (error, result) {
     if (error) {
+      //UPDATE TRANSACTION FOR FAILED TRANSACTION
+      transObj.pay_response = error;
+      transObj.transaction_status = 'Failed';
+      let updated = await(updateTransaction(transObj, params));
+      if (updated.hasOwnProperty('api_error_code')) {
+        return updated;
+      }
       console.log('>>>ERROR:: ', error);
       return error;
     } else {
+      //UPDATE TRANSACTION FOR SUCCESSFULL PAYMENT
+      transObj.pay_response = result.invoice || {};
+      transObj.transaction_status = 'Paid';
+      transObj.payment_status = true;
+      
+      let updated = await (updateTransaction(transObj, params));
+      if (updated.hasOwnProperty('api_error_code')) {
+        return updated;
+      }
+
+      //GET PLAN DETAILS WITH META_DATA FOR UPDATE OUR LOCAL USER-SUBSCRIPTION API
+      let plan = await(getPlanMetaData(result.subscription.plan_id, params));
+      let detail = {};
+      let module = _.groupBy(plan.meta_data.details, "module");
+      Object.keys(module).forEach(function(key) {
+        let service = _.groupBy(module[key], "service");
+        for (let i = 0; i < module[key].length; i++) {
+          detail[module[key][i].module] = {};
+          Object.keys(service).forEach(function(k) {
+            detail[module[key][i].module][k] = {};
+            for (let j = 0; j < service[k].length; j++) {
+              if (service[k][j].value !== '') {
+                let actionVal = parseInt(service[k][j].value);
+                detail[module[key][i].module][k][service[k][j].action] = actionVal;
+              }
+            }
+            if (Object.keys(detail[module[key][i].module][k]).length < 1) {
+              delete detail[module[key][i].module][k];
+            }
+          });
+        }
+      });
+      //CREATE PACKAGEOBJ TO UPDATE IN USER-SUBSCRIPTION
+      plan.price /= 100;
+      result.subscription.current_term_end = moment.unix(result.subscription.current_term_end).format();
+      
+      let packageObj = {
+        "details": detail,
+        "expiredOn": result.subscription.current_term_end,
+        "name": plan.name,
+        "price": plan.price,
+        "sub_id": result.subscription.id,
+        "time_unit": result.subscription.billing_period_unit,
+        "trans_id": result.invoice.linked_payments,
+        "userId": result.subscription.customer_id,
+        "validity": result.subscription.billing_period
+      }
+      //CALLING LOCAL API USER-SUBSCRIPTION TO ADD SUBSCRIPTION DETAILS LOCALLY
+      let userSub = await (addUserSubscription(packageObj, params));
+      if (userSub.hasOwnProperty('api_error_code')) {
+        return userSub;
+      }
+      //UPDATING TRANSACTION FOR 
+      transObj.transaction_status = 'user_updated';
+      updated = await (updateTransaction(transObj, params));
+      if (updated.hasOwnProperty('api_error_code')) {
+        return updated;
+      }
+      //GETING USER DETAILS FROM OUR USER DETAILS API TO UPDATE PACKAGE DETAILS OF USER
+      let userDetails = await (getUserDetails(params));
+      if (userDetails.hasOwnProperty('api_error_code')) {
+        return userDetails;
+      }
+
+      let planName = userSub.id.substr(userSub.id.length - 5) + "-" +  plan.name + "-" + moment(packageObj.expiredOn).format('DD-MMM-YYYY');
+      if (userDetails.data.package) {
+        userDetails.data.package[userSub.id] = { "subscriptionId": userSub.id, "role": "admin", "name": planName };
+      } else {
+        userDetails.data.package = {};
+        userDetails.data.package[userSub.id] = { "subscriptionId": userSub.id, "role": "admin", "name": planName };
+      }
+      let updateUserSub = await (updateUserPackageDetails(userDetails, result.subscription.customer_id, userSub.id, params));
+      if (updateUserSub.hasOwnProperty('api_error_code')) {
+        return updateUserSub;
+      }
+
+      transObj.transaction_status = 'completed';
+      updated = await (updateTransaction(transObj, params));
+      if (updated.hasOwnProperty('api_error_code')) {
+        return updated;
+      }
+
       console.log('>>>RESULT:: ', result);
       return result;
     }
-  });
+  }));
 });
 
 let updateSubscription = function (id, data, params) {
@@ -160,7 +257,59 @@ let initiateTransaction = function (data, params) {
   return app.service('transactions').create(transObj,{headers: {'Authorization': params.headers.authorization }}).then(res => {
     return res.id;
   }).catch(err => {
-    return { error: err.name, message: err.message };
+    return {api_error_code: err.name, error_msg: 'Internal server error.'}
+  });
+};
+
+let updateTransaction = function (transObj, params) {
+  transObj.updated_at = new Date();
+  let app = params.app;
+  console.log('Transaction updated', transObj);
+  return app.service('transactions').update(transaction_id, transObj, {headers: {'Authorization': params.headers.authorization }}).then(res => {
+    return res;
+  }).catch(err => {
+    return {api_error_code: err.name, error_msg: 'Internal server error.'};
+  });
+};
+
+let getPlanMetaData = function (plan_id, params) {
+  let app = params.app;
+  return app.service('cb-plan').get(plan_id, {headers: {'Authorization': params.headers.authorization }}).then(res => {
+    return res.data;
+  }).catch(err => {
+    return {api_error_code: err.name, error_msg: 'Internal server error.'};
+  });
+};
+
+let addUserSubscription = function (packageObj, params) {
+  let app = params.app;
+  packageObj.createdAt = new Date();
+  return app.service('user-subscription').create(packageObj).then(res => {
+    console.log('user-subscription id', res.id);
+    return res;
+  }).catch(err => {
+    return {api_error_code: err.name, error_msg: 'Internal server error.'};
+  });
+};
+
+let updateUserPackageDetails = function(userDetails, customer_id, defaultSubId, params) {
+  return axios.put(config.update_user_url + customer_id, {"package":userDetails.data.package, "defaultSubscriptionId": defaultSubId}, { headers: {'Content-Type': 'application/json', 'authorization': params.headers.authorization } }).then(res => {
+    console.log('User ',  customer_id, ' has subscribed  package successfully..!');
+    return res;
+  }).catch(err => {
+    return { error: err.name, message: 'Internal server error.' };
+  });
+};
+
+let getUserDetails = function (params) {
+  let options = {
+    uri: config.user_detail_url,
+    headers: { 'authorization': params.headers.authorization }
+  }
+  return rp(options).then(function (userDetail) {
+    return JSON.parse(userDetail);
+  }).catch(function (err) {
+    return {api_error_code: err.name, error_msg: 'Internal server error.'};
   });
 };
 
