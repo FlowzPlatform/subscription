11714794e011ec9355db6c8a85db384daca5aac8
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2018-06-04 15:45:43 +0530

    Update code with ESLint Syntax

--- service/src/services/checkout/checkout.hooks.js
+++ service/src/services/checkout/checkout.hooks.js
@@ -35,9 +35,9 @@ module.exports = {
 };
 
 function modify (hook) {
-  hook.params.app = hook.app
+  hook.params.app = hook.app;
   // console.log("hook......",hook)
   module.exports.apiHeaders = this.apiHeaders;
   // console.log("module.....",module.exports.apiHeaders.authorization)
-  hook.params.query.authorization = module.exports.apiHeaders.authorization
+  hook.params.query.authorization = module.exports.apiHeaders.authorization;
 }
