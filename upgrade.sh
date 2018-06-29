d02f5f7a7f0adc4a8551bf3728e80498836dd9e9
80017 <arpitapatel0611@gmail.com>
2018-06-29 12:52:39 +0530

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -13,6 +13,10 @@ then
     apiKey="$apiKey_master";
     site="$site_master";
     secret="$secret_master";
+    SERVICE_NAME="$SERVICE_NAME_MASTER";
+    BACKEND_HOST="$BACKEND_HOST_MASTER";
+    RDB_HOST="$RDB_HOST_MASTER";
+    RDB_PORT="$RDB_PORT_MASTER";
   }
 elif [ "$TRAVIS_BRANCH" = "develop" ]
 then
@@ -29,6 +33,10 @@ then
       apiKey="$apiKey_develop";
       site="$site_develop";
       secret="$secret_develop";
+      SERVICE_NAME="$SERVICE_NAME_DEVELOP";
+      BACKEND_HOST="$BACKEND_HOST_DEVELOP";
+      RDB_HOST="$RDB_HOST_DEVELOP";
+      RDB_PORT="$RDB_PORT_DEVELOP";
   }
 elif [ "$TRAVIS_BRANCH" = "staging" ]
 then
@@ -45,11 +53,15 @@ then
       apiKey="$apiKey_staging";
       site="$site_staging";
       secret="$secret_staging";
+      SERVICE_NAME="$SERVICE_NAME_STAGING";
+      BACKEND_HOST="$BACKEND_HOST_STAGING";
+      RDB_HOST="$RDB_HOST_STAGING";
+      RDB_PORT="$RDB_PORT_STAGING";
   }  
 else
   {
       echo "call $TRAVIS_BRANCH branch"
-      ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_QA":"$RANCHER_SECRETKEY_QA"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_QA/v2-beta/projects?name=QA" | jq '.data[].id' | tr -d '"'`
+      ENV_ID=`curl -u ""$RANCHER_ACCESSKEY_QA":"$RANCHER_SECRETKEY_QA"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL_QA/v2-beta/projects?name=Develop" | jq '.data[].id' | tr -d '"'`
       echo $ENV_ID
       USERNAME="$DOCKER_USERNAME";
       TAG="qa";
@@ -60,10 +72,14 @@ else
       apiKey="$apiKey_qa";
       site="$site_qa";
       secret="$secret_qa";
+      SERVICE_NAME="$SERVICE_NAME_QA";
+      BACKEND_HOST="$BACKEND_HOST_QA";
+      RDB_HOST="$RDB_HOST_QA";
+      RDB_PORT="$RDB_PORT_QA";
  }
 fi
 
-SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL/v2-beta/projects/$ENV_ID/services?name=subscription-backend-service-flowz" | jq '.data[].id' | tr -d '"'`
+SERVICE_ID=`curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" -X GET -H 'Accept: application/json' -H 'Content-Type: application/json' "$RANCHER_URL/v2-beta/projects/$ENV_ID/services?name=$SERVICE_NAME" | jq '.data[].id' | tr -d '"'`
 echo $SERVICE_ID
 
 curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
@@ -71,5 +87,5 @@ curl -u ""$RANCHER_ACCESSKEY":"$RANCHER_SECRETKEY"" \
 -H 'Accept: application/json' \
 -H 'Content-Type: application/json' \
 -d '{
-     "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/subscription_backend_service_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "machine=cluster-flowz"},"ports": ["3039:3039/tcp","4039:4039/tcp"],"environment": {"RDBHost": "'"$RDBHost"'","RDBPort": "'"$RDBPort"'","rdb":"'"$rdb"'","x_api_token":"'"$x_api_token"'","domainKey":"'"$DOMAINKEY"'","apiKey":"'"$apiKey"'","site":"'"$site"'","secret":"'"$secret"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3039,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
+     "inServiceStrategy":{"launchConfig": {"imageUuid":"docker:'$USERNAME'/subscription_backend_service_flowz:'$TAG'","kind": "container","labels":{"io.rancher.container.pull_image": "always","io.rancher.scheduler.affinity:host_label": "'"$BACKEND_HOST"'"},"ports": ["3039:3039/tcp","4039:4039/tcp"],"environment": {"RDBHost": "'"$RDB_HOST"'","RDBPort": "'"$RDB_PORT"'","rdb":"'"$rdb"'","x_api_token":"'"$x_api_token"'","domainKey":"'"$DOMAINKEY"'","apiKey":"'"$apiKey"'","site":"'"$site"'","secret":"'"$secret"'"},"healthCheck": {"type": "instanceHealthCheck","healthyThreshold": 2,"initializingTimeout": 60000,"interval": 2000,"name": null,"port": 3039,"recreateOnQuorumStrategyConfig": {"type": "recreateOnQuorumStrategyConfig","quorum": 1},"reinitializingTimeout": 60000,"responseTimeout": 60000,"strategy": "recreateOnQuorum","unhealthyThreshold": 3},"networkMode": "managed"}},"toServiceStrategy":null}' \
 $RANCHER_URL/v2-beta/projects/$ENV_ID/services/$SERVICE_ID?action=upgrade
